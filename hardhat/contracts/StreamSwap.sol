49ef44f1c9db4aae53edb4a83fa6eac3edab1f66
jdonosocoding <javier.donoso@nuilea.com>
2022-05-18 17:37:03 +0200

    contract compile and liquidity test

--- hardhat/contracts/StreamSwap.sol
+++ hardhat/contracts/StreamSwap.sol
@@ -1,12 +1,57 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "hardhat/console.sol";
 
-contract StreamSwap {
+contract StreamSwap is ERC20 {
+  address token1;
+  address token2;
+  uint public ratio;
+  uint constant  public DECIMALS_RATIO = 1000000;
+  constructor(address _token1, address _token2) ERC20("SSW", "Stream Swap LP") {
+    require(_token1 != address(0), "Token1 address passed is a null address");
+    require(_token2 != address(0), "Token2 address passed is a null address");
+    token1 = _token1;
+    token2 = _token2;
+  }
 
-    
-    constructor() {
-   
-    }
+  // DEPOSIT
+function addInitialLiquidity(uint _amountToken1, uint _amountToken2) public {
+    ratio = _amountToken1 * DECIMALS_RATIO/(_amountToken2);
+    console.log(ratio);
+}
+
+
+  // WIDTHDRAW
+
+  // SWAP
+
+  // HELPERS
+
+
+/**
+* @dev Returns the amount of `Token1 held by the contract
+*/
+function getLPTokens() public view returns (uint) {
+
+
+    return 2;
+}
+
+
+/**
+* @dev Returns the amount of `Token1 held by the contract
+*/
+function getReserveToken1() public view returns (uint) {
+    return ERC20(token1).balanceOf(address(this));
+}
+
+/**
+* @dev Returns the amount of `Token1 held by the contract
+*/
+function getReserveToken2() public view returns (uint) {
+    return ERC20(token2).balanceOf(address(this));
+}
+
 }
--- hardhat/test/streamSwap_spec.ts
+++ hardhat/test/streamSwap_spec.ts
@@ -1,12 +1,43 @@
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { expect } from "chai";
 import { ethers } from "hardhat";
+import { initEnv } from "../helpers/utils";
+import * as hre from 'hardhat';
+import { BaseProvider, TransactionReceipt } from '@ethersproject/providers';
+import { StreamSwap, StreamSwap__factory } from "../typechain-types";
 
 describe("Stream Swap", function () {
-  it("Should Start", async function () {
-    const StreamSwap = await ethers.getContractFactory("StreamSwap");
-    const streamSwap = await StreamSwap.deploy();
-    await streamSwap.deployed();
 
- 
+  let streamSwapContract:StreamSwap
+
+  let deployer: SignerWithAddress;
+  let user1: SignerWithAddress;
+  let user2: SignerWithAddress;
+  let user3: SignerWithAddress;
+  let provider: BaseProvider;
+
+  beforeEach(async () => {
+    [deployer, user1, user2, user3] = await initEnv(hre);
+
+    provider =   hre.ethers.provider;
+
+    streamSwapContract =  await new StreamSwap__factory(deployer).deploy("0x5D8B4C2554aeB7e86F387B4d6c00Ac33499Ed01f","0x42bb40bF79730451B11f6De1CbA222F17b87Afd7");
+
+  })
+
+
+  it("Should Start with zero tokens", async function () {
+    expect(await streamSwapContract.totalSupply()).to.equal(0);
+
   });
+
+  it('should add liquidity and set correct ratio', async function () {
+    await streamSwapContract.addInitialLiquidity(450,300);
+    let ratio =  await streamSwapContract.ratio();
+    expect(ratio.toString()).to.equal("1500000")
+    let decimals = await streamSwapContract.DECIMALS_RATIO();
+    expect(+ratio.toString()/+decimals.toString()).to.equal(1.5)
+  });
+
+
 });
